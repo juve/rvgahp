6a8e28e5f5aaf16efd940cf9f3e86aebb2c2db4d
Gideon Juve <gideon@isi.edu>
2015-12-21 07:06:38 -0800

    Add arguments to CE for daemonize and logfile

--- TODO.md
+++ TODO.md
@@ -1,5 +1,3 @@
 TODO
 ====
-1. Add support for daemonizing CE and writing to a log file (as cmdline args)
-1. Allow CE to use regular condor_config
 1. Write man pages
--- rvgahp_ce.c
+++ rvgahp_ce.c
@@ -8,10 +8,12 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <libgen.h>
 #include <netdb.h>
 #include <signal.h>
 #include <math.h>
+#include <fcntl.h>
 
 #include "common.h"
 
@@ -23,21 +25,22 @@ void sigterm(int sig) {
 }
 
 void usage() {
-    fprintf(stderr, "Usage: %s\n", argv0);
-    fprintf(stderr, "This command has no options\n");
+    fprintf(stderr, "Usage: %s [-d] [-l LOGFILE]\n", argv0);
+    fprintf(stderr, "  -d          Daemonize\n");
+    fprintf(stderr, "  -l LOGFILE  Write logging messages to LOGFILE\n");
 }
 
 int set_condor_config() {
     char *homedir = getenv("HOME");
     if (homedir == NULL) {
-        log(stderr, "ERROR HOME is not set in environment");
+        fprintf(stderr, "ERROR HOME is not set in environment");
         return -1;
     }
 
     char config_file[BUFSIZ];
     snprintf(config_file, BUFSIZ, "%s/%s", homedir, ".rvgahp/condor_config.rvgahp");
     if (access(config_file, R_OK) < 0) {
-        log(stderr, "ERROR Cannot find config file %s", config_file);
+        fprintf(stderr, "ERROR Cannot find config file %s", config_file);
         return -1;
     }
 
@@ -83,7 +86,6 @@ int loop() {
     if (b < 0) {
         log(stderr, "ERROR read from SSH failed: %s\n", strerror(errno));
         /* This probably happened because the SSH process died */
-        /* TODO Check to see if ssh is running */
         goto error;
     }
     if (b == 0) {
@@ -151,18 +153,69 @@ error:
 int main(int argc, char** argv) {
     argv0 = basename(strdup(argv[0]));
 
-    if (argc > 1) {
-        fprintf(stderr, "Invalid argument: %s\n", argv[1]);
+    /* Parse arguments */
+    char *logfilename = NULL;
+    int daemonize = 0;
+    int c;
+    while ((c = getopt (argc, argv, "dl:")) != -1) {
+        switch (c) {
+        case 'd':
+            daemonize = 1;
+            break;
+        case 'l':
+            logfilename = optarg;
+            break;
+        case '?':
+            if (optopt == 'l') {
+                fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+            } else {
+                fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+            }
+            return 1;
+        default:
+            abort();
+        }
+    }
+
+    if (argc > optind) {
+        fprintf(stderr, "Invalid argument: %s\n", argv[optind]);
         usage();
         exit(1);
     }
-
-    log(stdout, "%s starting...\n", argv0);
 
     /* Set up configuration */
     if (set_condor_config() < 0) {
         exit(1);
     }
+
+    /* Redirect stdio to logfile if specified */
+    if (logfilename != NULL) {
+        int logfile = open(logfilename, O_WRONLY|O_CREAT|O_APPEND, 0600);
+        if (logfile < 0) {
+            fprintf(stderr, "ERROR opening logfile %s: %s\n", logfilename, strerror(errno));
+            exit(1);
+        }
+        close(1);
+        close(2);
+        dup(logfile);
+        dup(logfile);
+    }
+
+    if (daemonize) {
+        setsid();
+        pid_t pid = fork();
+        if (pid < 0) {
+            fprintf(stderr, "ERROR forking: %s\n", strerror(errno));
+            exit(1);
+        } else if (pid == 0) {
+            /* Child goes on */
+        } else {
+            /* Parent exits */
+            exit(0);
+        }
+    }
+
+    log(stdout, "%s starting...\n", argv0);
     log(stdout, "Config file: %s\n", getenv("CONDOR_CONFIG"));
 
     signal(SIGCHLD, SIG_IGN);
@@ -176,7 +229,7 @@ int main(int argc, char** argv) {
             failures++;
             double sleeptime = pow(2, failures);
             if (sleeptime > 300) sleeptime = 300;
-            log(stdout, "Failure occured, waiting %d seconds to respawn\n", (int)sleeptime);
+            log(stderr, "Failure occured, waiting %d seconds to respawn\n", (int)sleeptime);
             sleep(sleeptime);
         }
     }
